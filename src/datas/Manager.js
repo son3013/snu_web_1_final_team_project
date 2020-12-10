b58ec78459a552059061bf2699bf737fdfc9c228
Paul Lee <paullee20204@snu.ac.kr>
2020-12-10 17:29:29 +0900

    :zap: Create MonsterManager & ItemManager

--- src/datas/Manager.js
+++ src/datas/Manager.js
@@ -32,6 +32,43 @@ class MapManager extends Manager {
     return this.fields[`${x}_${y}`];
   }
 }
+
+class MonsterManager extends Manager {
+  constructor(datas) {
+    super();
+    this.id = 1;
+    this.monsters = [];
+
+    datas.forEach((monster) => {
+      this.monsters.push(monster);
+    });
+  }
+
+  getRandomMonster() {
+    const arrayLength = this.monsters.length;
+    const rand_num = Math.floor(Math.random() * (arrayLength)); 
+    return this.monsters[rand_num];
+  }
+}
+
+class ItemManager extends Manager {
+  constructor(datas) {
+    super();
+    this.id = 1;
+    this.items = [];
+
+    datas.forEach((item) => {
+      this.items.push(item);
+    });
+  }
+
+  getRandomItem() {
+    const arrayLength = this.items.length;
+    const rand_num = Math.floor(Math.random() * (arrayLength)); 
+    return this.items[rand_num];
+  }
+}
+
 const constantManager = new ConstantManager(
   JSON.parse(fs.readFileSync(__dirname + "/constants.json"))
 );
@@ -40,7 +77,17 @@ const mapManager = new MapManager(
   JSON.parse(fs.readFileSync(__dirname + "/map.json"))
 );
 
+const monsterManager = new MonsterManager(
+  JSON.parse(fs.readFileSync(__dirname + "/monsters.json"))
+);
+
+const itemManager = new ItemManager(
+  JSON.parse(fs.readFileSync(__dirname + "/items.json"))
+);
+
 module.exports = {
   constantManager,
-  mapManager
+  mapManager,
+  monsterManager,
+  itemManager
 };
